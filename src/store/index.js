deadf28428ca59d424451d602b613f0a318b886c
Neeraj Kumar <neeraj.17011996@gmail.com>
2019-10-03 23:41:44 +0530

    - redux-saga integrated

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,21 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {persistReducer, persistStore} from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import rootSaga from '../sagas';
 import rootReducer from './reducers';
 
 const persistConfig = {
   key: 'root',
   storage,
 };
+const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default () => {
-  let store = createStore(persistedReducer);
+  sagaMiddleware.run(rootSaga);
+  let store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
   let persistor = persistStore(store);
   return {store, persistor};
 };
