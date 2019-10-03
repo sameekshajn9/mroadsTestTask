2a697b7e5e64a8f972b23faadc0b7bd94c8268cd
Neeraj Kumar <neeraj.17011996@gmail.com>
2019-10-03 23:38:06 +0530

    - redux integrated

--- index.js
+++ index.js
@@ -4,6 +4,17 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import appStore from './src/store';
+const {store, persistor} = appStore();
+
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => (
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+));
