652135a5408de467bf1609e2f687e23ecac4c854
Neeraj Kumar <neeraj.17011996@gmail.com>
2019-10-09 21:23:04 +0530

    - minor issue fixes

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 /**
  * @format
  */
-import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
-import appStore from './src/store';
-const {store, persistor} = appStore();
 
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => (
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
-));
+AppRegistry.registerComponent(appName, () => App);
